1dfba8ba9990ff7edcc29e764a3e67d18f8ce6b8
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-19 14:37:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,16 @@
 class Dog
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
+
+  def name=(dogs_name)
+    @name = dogs_name
   end
+
   def name
     @name
   end
+  def breed=(breed)
+    @breed = breed
+  end
+
   def breed
     @breed
   end
